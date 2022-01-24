899e407bdd6387625192b6bdb130aed328d7ee8a
viktornuts <viktornuts@gmail.com>
2022-01-24 17:44:06 +0500

    4st commit

---
+++ src/test/java/tests/PracticeFormTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFromTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Viktor");
+        $("#lastName").setValue("Slon");
+        $("#userEmail").setValue("viktornuts@gmail.com");
+        $("[for='gender-radio-1']").click();
+        $("#userNumber").setValue("8955245541");
+
+        $("#dateOfBirthInput").click();
+        $("[class='react-datepicker__month-select']").selectOption("June");
+        $("[class='react-datepicker__year-select']").selectOption("1990");
+        $("[class*='react-datepicker__day--021']").click();
+
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+
+        $("[for='hobbies-checkbox-1']").click();
+        $("[for='hobbies-checkbox-2']").click();
+
+        File lesson = new File("src/test/java/guru/qa/files/lesson1.png");
+        String path = lesson.getAbsolutePath();
+        $("#uploadPicture").sendKeys(path);
+
+        $("[placeholder='Current Address']").setValue("Nikolaya Shishka 21");
+        $("[placeholder='Current Address']").scrollIntoView(true);
+        $("#react-select-3-input").setValue("Raj").pressEnter();
+        $("#react-select-4-input").setValue("Jaise").pressEnter();
+        $("#submit").click();
+
+        $$x("//*[@class='modal-body']//td[2]").shouldHave(CollectionCondition.exactTexts(
+                "Viktor Slon", "viktornuts@gmail.com", "Male", "8955245541", "21 June,1990",
+                "English, Maths", "Sports, Reading", "lesson1.png", "Nikolaya Shishka 21", "Rajasthan Jaiselmer"));
+    }
+
+
+}

307a35714e71fa20ecdf670d2a0771ed0b15368c
viktornuts <viktonuts@gmail.com>
2022-02-22 12:14:37 +0500

    add owner example

---
+++ src/test/java/Owner/OwnerTests.java
@@ -0,0 +1,17 @@
+package Owner;
+
+import org.junit.jupiter.api.Test;
+
+public class OwnerTests {
+
+    @Test
+        void loginTest() {
+        String login = "some_login";
+        String pass = "some_pass";
+
+        System.out.println("Login :" + login);
+        System.out.println("Pass :" + pass);
+
+}
+
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,23 +1,32 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static java.lang.String.format;
 
 public class TestBase {
+
 
     @BeforeAll
     static void setUp() {
 
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+        String login = config.login();
+        String password = config.password();
+        String remoteUrl = format("https://%s:%s@selenoid.autotests.cloud/wd/hub", login, password);
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = remoteUrl;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
